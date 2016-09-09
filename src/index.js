bda614eaf178aa8e4a1f587b7a54058c92f193f2
Erik Timmers <erik@ultimate>
2016-09-09 17:41:44 +0200

    skeleton

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import 'console-polyfill';
 import {Config} from './config';
 
 export {Firebase} from './firebase';
+
+// TODO
+// 
+// import firebase from 'persistence-plugin-firebase';
+//
+// baseConfig
+//   .configure({/* baseUrl from plugin */})
+//   .plugin(firebase({
+//     apiKey: 'AIzaSyC373ylG9TII68H3Xua_nS2dVI0ihaCgZs',
+//     authDomain: 'warper.firebaseapp.com',
+//     databaseURL: 'https://warper.firebaseio.com',
+//     storageBucket: 'project-313180311630044673.appspot.com'
+//   });
